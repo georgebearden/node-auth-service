36851ea8cb1a82d6211c0ee7773451eb2647052b
georgebearden <george@goerden.com>
2015-06-29 14:55:09 -0400

    Simplifying the way username exists checks are being done.

---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+var User = require('../models/user');
+var express = require('express');
+var router = express.Router();
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-var auth = require('../models/auth');
 var User = require('../models/user');
 var express = require('express');
 var router = express.Router();
@@ -9,7 +8,8 @@ router.post('/', function(req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
 	
-	auth.usernameExists(username, function(err, userExists) {
+	User.findOne({username: username}, function(err, userExists) {
+		if (err) throw err;
 		
 		if (userExists) {
 			res.status(400);
@@ -25,6 +25,7 @@ router.post('/', function(req, res) {
 				if (err) throw err;
 				
 				console.log('created user: ' + username);
+				
 				res.send({token: "lakjdsflkajsdf"});
 			});
 		}
